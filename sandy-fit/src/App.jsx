fe8e5f3a1f0a3165997b57910cdd4e8a0eb756cb
claranceJ <jacktonec@gmail.com>
2024-09-29 13:23:33 +0300

    feat: include the new components

--- sandy-fit/src/App.jsx
+++ sandy-fit/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Hero from './components/Hero'
+import Generator from './components/Generator'
+import Workout from './components/Workout'
 
 function App() {
   return (
-    <div className='bg-slate-800'>App</div>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base' >
+      <Hero/>
+      <Generator/>
+      <Workout/>
+
+    
+    </main>
   )
 }
 
